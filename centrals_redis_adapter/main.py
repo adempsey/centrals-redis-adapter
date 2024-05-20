c14ccdd92345b7f8f7918d434dd63b9f3557e669
Andrew Dempsey <amd0331@gmail.com>
2024-05-20 10:36:26 -0400

    lsiten for commands

--- centrals_redis_adapter/main.py
+++ centrals_redis_adapter/main.py
@@ -1,12 +1,25 @@
-import redis
+import asyncio
+import redis.asyncio as redis
 from cerebus import cbpy
+from enum import Enum
 
-def main():
-    redis_client = redis.Redis("localhost", 6379)
-    # redis_client.xadd("CENTRALS_COMMANDS", {"key": "value"})
+Commands = Enum("Commands", ["START", "STOP"])
+COMMAND_STREAM = "CENTRALS_COMMANDS"
+
+async def listener(channel: redis.client.PubSub):
     while True:
-        result = redis_client.xread({"CENTRALS_COMMANDS": b"0-0"})
-        print(result[0])
+        message = await channel.get_message(ignore_subscribe_messages=True)
+        if message is not None:
+            command = message["data"].decode()
+            print(command)
+
+async def main():
+    redis_client = redis.Redis(host="localhost", port=6379)
+
+    async with redis_client.pubsub() as pubsub:
+        await pubsub.subscribe(COMMAND_STREAM)
+        future = asyncio.create_task(listener(pubsub))
+        await future
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
